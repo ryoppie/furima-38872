02076f1e7cf55ff62ca12d6c028f3ab976161d78
ryoppie <rmsidejob0913@gmail.com>
2023-04-18 00:53:50 +0900

    ユーザー管理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  # deviseコントローラー時に下記メソッドを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,5 +10,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]   # 環境変数を読み込む記述で設定
     end
+  end
+
+  # 新規登録時、emailとencrypted_password以外もストロングパラメーターとして許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
 end
