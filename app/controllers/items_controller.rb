45d0666fd0810d72ac2f4bb8cec3a8421fe797fe
ryoppie <rmsidejob0913@gmail.com>
2023-04-25 21:36:07 +0900

    first_commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,36 @@
+class ItemsController < ApplicationController
+  # ログインしていないユーザーはログインページに促す
+  before_action :authenticate_user!, except: [:index, :show]
+
+  # 重複処理をまとめる
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'itemes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "itemes#index"
+  root to: "items#index"
+  resources :items 
 end
